b0b1f59c04fa5e0820b9fe363bf32a816d52a1e8
SNSIMANTO <snsimanto.cse.sub@gmail.com>
2025-03-07 12:01:06 +0600

    1st project

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "myfirstclass.h"
+using namespace std;
+
+int main()
+{
+    MyFirstClass ob1;
+    MyFirstClass ob2;
+    ob2.display();
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ MyFirstProject/myfirstclass.cpp
@@ -0,0 +1,15 @@
+#include "myfirstclass.h"
+#include<iostream>
+using namespace std;
+
+MyFirstClass::MyFirstClass()
+{
+    //ctor
+    cout<<"Inside the constructor."<<endl;
+}
+
+
+void MyFirstClass :: display()
+{
+    cout<<"The End"<<endl;
+}
